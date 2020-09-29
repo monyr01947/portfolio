790ec1af9ccb1d441721a62fef861df450dbbf05
monyr01947 <70470152+monyr01947@users.noreply.github.com>
2020-09-29 16:25:40 +0600

    Updates

---
+++ precache-manifest.dd6e1b6ad1494ca28405aefe254f63f6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dcdd4720276529583a37d41610de4da",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "337698913daabe199dbe",
+    "url": "/portfolio/static/css/2.fd971cca.chunk.css"
+  },
+  {
+    "revision": "0c85d723b24bf3cda205",
+    "url": "/portfolio/static/css/main.cd5750a1.chunk.css"
+  },
+  {
+    "revision": "337698913daabe199dbe",
+    "url": "/portfolio/static/js/2.365cbe68.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.365cbe68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c85d723b24bf3cda205",
+    "url": "/portfolio/static/js/main.0ae3b53c.chunk.js"
+  },
+  {
+    "revision": "81610c46db9406910ddc",
+    "url": "/portfolio/static/js/runtime-main.a77b804c.js"
+  },
+  {
+    "revision": "0c79ea03f54a0df0b6439f56fd3010f5",
+    "url": "/portfolio/static/media/333.0c79ea03.jpg"
+  },
+  {
+    "revision": "917a4c1a3bcc56d6aea111526611b851",
+    "url": "/portfolio/static/media/about_bg.917a4c1a.png"
+  },
+  {
+    "revision": "5644dd3bdab7a976ddbb67bcbd3e35eb",
+    "url": "/portfolio/static/media/blog-1.5644dd3b.jpg"
+  }
+]);
